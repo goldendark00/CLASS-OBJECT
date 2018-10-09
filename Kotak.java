89d7cc9184898baafc413df9bf1fd8c856616f0b
goldendark00 <43660882+goldendark00@users.noreply.github.com>
2018-10-09 23:37:55 +0800

    Create Kotak.java

---
+++ Kotak.java
@@ -0,0 +1,36 @@
+public class Kotak {
+	private double Panjang, Lebar, Luas, Keliling;
+
+	public double getPanjang() {
+		return Panjang;
+	}
+
+	public void setPanjang(double panjang) {
+		Panjang = panjang;
+	}
+
+	public double getLebar() {
+		return Lebar;
+	}
+
+	public void setLebar(double lebar) {
+		Lebar = lebar;
+	}
+
+	public double getLuas() {
+		return Luas;
+	}
+
+	public void setLuas(double luas) {
+		Luas = luas;
+	}
+
+	public double getKeliling() {
+		return Keliling;
+	}
+
+	public void setKeliling(double keliling) {
+		Keliling = keliling;
+
+	}
+}
