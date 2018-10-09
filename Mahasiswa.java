4d3e6d13ed63b42e2faa716a3c844a99df053d3e
goldendark00 <43660882+goldendark00@users.noreply.github.com>
2018-10-09 23:38:29 +0800

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,48 @@
+public class Mahasiswa {
+	private String Nama, Nim, Alamat, GolonganDarah, Status;
+	private int TinggiBadan, BeratBadan;
+
+	public String getNama() {
+		return Nama;
+	}
+	public void setNama(String nama) {
+		Nama = nama;
+	}
+	public String getNim() {
+		return Nim;
+	}
+	public void setNim(String nim) {
+		Nim = nim;
+	}
+	public String getAlamat() {
+		return Alamat;
+	}
+	public void setAlamat(String alamat) {
+		Alamat = alamat;
+	}
+	public String getGolonganDarah() {
+		return GolonganDarah;
+	}
+	public void setGolonganDarah(String golonganDarah) {
+		GolonganDarah = golonganDarah;
+	}
+	public String getStatus() {
+		return Status;
+	}
+	public void setStatus(String status) {
+		Status = status;
+	}
+	public int getTinggiBadan() {
+		return TinggiBadan;
+	}
+	public void setTinggiBadan(int tinggiBadan) {
+		TinggiBadan = tinggiBadan;
+	}
+	public int getBeratBadan() {
+		return BeratBadan;
+	}
+	public void setBeratBadan(int beratBadan) {
+		BeratBadan = beratBadan;
+
+	}
+}
